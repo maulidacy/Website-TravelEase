3c4c91c8dbaf03bb826acadf135d8f512935f40c
maulidacy <cahyamaulida011@gmail.com>
2025-05-09 20:05:38 +0700

    update

--- frontend/script.js
+++ frontend/script.js
@@ -3,23 +3,32 @@ const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
 
 const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
+    navbar.classList.toggle("active");
+    mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
 //============================== Navbar end ===============================
 
-
 //============================== slideshow Home section start ===============================
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-
-function changeSlide() {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  document.querySelector('.slider').style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-setInterval(changeSlide, 5000);  // Change image every 5 seconds
-
+document.addEventListener('DOMContentLoaded', function() {
+  const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.slide');
+  let currentIndex = 0;
+  const slideCount = slides.length;
+  
+  // Atur lebar slider berdasarkan jumlah slide
+  slider.style.width = `${slideCount * 100}%`;
+  
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slideCount;
+    updateSlider();
+  }
+  
+  function updateSlider() {
+    const translateValue = -currentIndex * (100 / slideCount);
+    slider.style.transform = `translateX(${translateValue}%)`;
+  }
+  
+  setInterval(nextSlide, 3000);
+});
 //============================== slideshow Home section end ===============================
