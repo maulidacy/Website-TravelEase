a6511cade5830e8a00586658569bd0829eff6f0d
maulidacy <cahyamaulida011@gmail.com>
2025-05-09 18:31:41 +0700

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,4 @@
+//============================== Navbar start ===============================
 const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
 
@@ -6,3 +7,19 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+//============================== Navbar end ===============================
+
+
+//============================== slideshow Home section start ===============================
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function changeSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  document.querySelector('.slider').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(changeSlide, 5000);  // Change image every 5 seconds
+
+//============================== slideshow Home section end ===============================
