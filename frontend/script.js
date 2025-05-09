413dc474448aa4c1caab52a6803bb9a9c8ff1cdb
maulidacy <cahyamaulida011@gmail.com>
2025-05-09 20:24:03 +0700

    update

--- frontend/script.js
+++ frontend/script.js
@@ -3,10 +3,11 @@ const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
 
 const toggleNav = () => {
-    navbar.classList.toggle("active");
-    mobileNav.classList.toggle("hamburger-active");
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
 //============================== Navbar end ===============================
 
 //============================== slideshow Home section start ===============================
